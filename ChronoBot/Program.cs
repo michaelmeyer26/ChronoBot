b85d18b4a30de9fbf84fcb7980076d1f11d0eade
Michael Meyer <michael.meyer@improving.com>
2021-08-09 10:36:24 -0500

    Initial Commit

---
+++ ChronoBot/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace ChronoBot
+{
+    public class Program
+    {
+
+        private DiscordSocketClient _client;
+
+        static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            var token = File.ReadAllText("token.txt");
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until program is closed.
+            await Task.Delay(-1);
+        }
+
+        // Logs message to console
+        // Replace with logging messages to a text file eventually
+        // Figure out a better long-term solution after that
+        private Task Log(LogMessage msg)
+        {
+            Console.Write(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+
+    }
+}
